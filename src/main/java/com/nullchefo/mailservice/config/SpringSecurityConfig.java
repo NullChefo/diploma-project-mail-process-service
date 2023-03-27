e57ec19e48a6f76f2e81746c492bc8b082a231b3
nullchefo <stefan.kehayov.96@gmail.com>
2023-03-27 17:36:53 +0300

    Update SpringSecurityConfig

---
+++ src/main/java/com/nullchefo/mailservice/config/SpringSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.nullchefo.mailservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http    // TODO fix this
+				.csrf().disable()
+				//	.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				//	.and()
+				.authorizeHttpRequests()
+				.requestMatchers("/actuator/**" ).permitAll()
+				.anyRequest().authenticated();
+		http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+		return http.build();
+	}
+}
