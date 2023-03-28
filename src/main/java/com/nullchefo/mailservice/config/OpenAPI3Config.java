4f187f613d80bcda04a617b5830f87ba2f9b2ece
nullchefo <stefan.kehayov.96@gmail.com>
2023-03-28 17:37:56 +0300

    Update SpringSecurityConfig for OpenAPI

---
+++ src/main/java/com/nullchefo/mailservice/config/OpenAPI3Config.java
@@ -0,0 +1,34 @@
+package com.nullchefo.mailservice.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.OAuthScope;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+//TODO fix server for development
+@OpenAPIDefinition(servers = { @Server(url = "https://backend.nulchefo.com/mail-process") },  info = @Info(title = "Mail Process Service APIs", version = "v0.0.1", description = "Mail Process Service API", contact = @Contact(url = "https://nullchefo.com", name = "Fred", email = "stefank.dev.acc@gmail.com")))
+@SecurityScheme(
+		name = "security_auth",
+		type = SecuritySchemeType.OAUTH2,
+		//		in = SecuritySchemeIn.HEADER,
+		//		bearerFormat = "jwt",
+		flows = @OAuthFlows(
+				authorizationCode = @OAuthFlow(
+						authorizationUrl = "https://auth.nullchefo.com/oauth2/authorize",
+						tokenUrl = "https://auth.nullchefo.com/oauth/token",
+						scopes = {
+								@OAuthScope(name = "openid", description = "openid scope"),
+								@OAuthScope(name = "mail-process.read", description = "with this scope, the user can access every get request"),
+								@OAuthScope(name = "mail-process.write", description = "with this scope, the user can access every post, put and delete request")
+						}
+				)
+		)
+)
+
+public class OpenAPI3Config {
+}
