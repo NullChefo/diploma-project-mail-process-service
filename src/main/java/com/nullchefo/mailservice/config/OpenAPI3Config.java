1412ffee0638fc726a2c72b7eb4c3321879d788f
nullchefo <stefan.kehayov.96@gmail.com>
2023-03-29 00:33:19 +0300

    Update OpenApi to use token

--- src/main/java/com/nullchefo/mailservice/config/OpenAPI3Config.java
+++ src/main/java/com/nullchefo/mailservice/config/OpenAPI3Config.java
@@ -12,6 +12,19 @@ import io.swagger.v3.oas.annotations.servers.Server;
 
 //TODO fix server for development
 @OpenAPIDefinition(servers = { @Server(url = "https://backend.nullchefo.com/mail-process") },  info = @Info(title = "Mail Process Service APIs", version = "v0.0.1", description = "Mail Process Service API", contact = @Contact(url = "https://nullchefo.com", name = "Stefan", email = "stefank.dev.acc@gmail.com")))
+/*
+@SecurityScheme(name = "OAuth2", type = SecuritySchemeType.OAUTH2,
+				flows = @OAuthFlows(authorizationCode = @OAuthFlow(
+						authorizationUrl = "http://localhost:9000",
+						tokenUrl = "http://localhost:9000/oauth/token",
+						scopes = {
+								@OAuthScope(name = "openid", description = "Read Access"),
+								@OAuthScope(name = "read", description = "Read Access"),
+								@OAuthScope(name = "write", description = "Write Access")
+						})))
+
+
+
 @SecurityScheme(
 		name = "security_auth",
 		type = SecuritySchemeType.OAUTH2,
@@ -23,12 +36,19 @@ import io.swagger.v3.oas.annotations.servers.Server;
 						tokenUrl = "https://auth.nullchefo.com/oauth2/token",
 						scopes = {
 								@OAuthScope(name = "openid", description = "openid scope"),
-								@OAuthScope(name = "mail-process.read", description = "with this scope, the user can access every get request"),
-								@OAuthScope(name = "mail-process.write", description = "with this scope, the user can access every post, put and delete request")
+								@OAuthScope(name = "social_media.read", description = "with this scope, the user can access every get request"),
+								@OAuthScope(name = "social_media.write", description = "with this scope, the user can access every post, put and delete request")
 						}
 				)
 		)
 )
 
+ */
+@SecurityScheme(
+		name = "Bearer Authentication",
+		type = SecuritySchemeType.HTTP,
+		bearerFormat = "JWT",
+		scheme = "bearer"
+)
 public class OpenAPI3Config {
 }
