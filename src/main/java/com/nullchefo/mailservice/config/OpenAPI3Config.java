abd8c22a4ade4c4fd1cc37c2562956680ee140ec
nullchefo <stefan.kehayov.96@gmail.com>
2023-04-01 00:06:49 +0300

    Trying jwks configuration

--- src/main/java/com/nullchefo/mailservice/config/OpenAPI3Config.java
+++ src/main/java/com/nullchefo/mailservice/config/OpenAPI3Config.java
@@ -4,14 +4,12 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.OAuthFlow;
-import io.swagger.v3.oas.annotations.security.OAuthFlows;
-import io.swagger.v3.oas.annotations.security.OAuthScope;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 //TODO fix server for development
-@OpenAPIDefinition(servers = { @Server(url = "https://backend.nullchefo.com/mail-process") },  info = @Info(title = "Mail Process Service APIs", version = "v0.0.1", description = "Mail Process Service API", contact = @Contact(url = "https://nullchefo.com", name = "Stefan", email = "stefank.dev.acc@gmail.com")))
+@OpenAPIDefinition(servers = {
+		@Server(url = "https://backend.nullchefo.com/mail-process") }, info = @Info(title = "Mail Process Service APIs", version = "v0.0.1", description = "Mail Process Service API", contact = @Contact(url = "https://nullchefo.com", name = "Stefan", email = "stefank.dev.acc@gmail.com")))
 /*
 @SecurityScheme(name = "OAuth2", type = SecuritySchemeType.OAUTH2,
 				flows = @OAuthFlows(authorizationCode = @OAuthFlow(
