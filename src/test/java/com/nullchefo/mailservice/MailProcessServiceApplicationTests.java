5aa303334723533280561546197a866b0f936f09
nullchefo <stefan.kehayov.96@gmail.com>
2023-04-27 00:23:32 +0300

    testcontainers and more

--- src/test/java/com/nullchefo/mailservice/MailProcessServiceApplicationTests.java
+++ src/test/java/com/nullchefo/mailservice/MailProcessServiceApplicationTests.java
@@ -1,10 +1,31 @@
 package com.nullchefo.mailservice;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 @SpringBootTest
+@Testcontainers
 class MailProcessServiceApplicationTests {
+
+	@Container
+	private final PostgreSQLContainer<?> postgresqlContainer = new PostgreSQLContainer()
+			.withDatabaseName("foo")
+			.withUsername("foo")
+			.withPassword("secret");
+
+	@Before
+	void before() {
+		postgresqlContainer.start();
+	}
+
+	@After
+	void after() {
+		postgresqlContainer.stop();
+	}
 
 	@Test
 	void contextLoads() {
