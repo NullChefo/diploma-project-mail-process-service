f09b9631ca35fc7a63f42f9b2075c4686171c2c7
nullchefo <stefan.kehayov.96@gmail.com>
2023-05-04 22:39:56 +0300

    added more functionality on front end back end

--- src/test/java/com/nullchefo/mailservice/MailProcessServiceApplicationTests.java
+++ src/test/java/com/nullchefo/mailservice/MailProcessServiceApplicationTests.java
@@ -2,6 +2,7 @@ package com.nullchefo.mailservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 @SpringBootTest
 class MailProcessServiceApplicationTests extends AbstractTestcontainers {
 
